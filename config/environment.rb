96c37bbd68f85467218fce2bd963d298a8081110
David Dollar <ddollar@gmail.com>
2009-03-25 15:45:03 -0400

    on the road to full testing

--- config/environment.rb
+++ config/environment.rb
@@ -5,8 +5,13 @@ Rails::Initializer.run do |config|
 
   config.time_zone = 'UTC'
 
+  # authentication
   config.gem 'thoughtbot-clearance',    :version => '>= 0.5.4',  :lib => 'clearance'
+
+  # testing
   config.gem 'thoughtbot-shoulda',      :version => '>= 2.10.1', :lib => 'shoulda'
   config.gem 'thoughtbot-factory_girl', :version => '>= 1.2.0',  :lib => 'factory_girl'
+  config.gem 'webrat',                  :version => '>= 0.4.0'
+  config.gem 'cucumber',                :version => '>= 0.2.2'
 
 end
