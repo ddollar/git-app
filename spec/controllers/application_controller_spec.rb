0eb1aec765cc81dc3ae4c7ae342f4ed6409365f9
David Dollar <ddollar@gmail.com>
2009-04-01 15:24:42 -0400

    moved authentication helper method tests to applicationcontrller

--- spec/controllers/application_controller_spec.rb
+++ spec/controllers/application_controller_spec.rb
@@ -2,4 +2,24 @@ require File.join(File.dirname(__FILE__), '..', 'spec_helper')
 
 describe ApplicationController do
 
+  describe '#current_user' do
+    it 'should return the currently logged in user' do
+      user = Factory(:user)
+      session[:user] = user
+      controller.current_user.should == user
+    end
+  end
+
+  describe '#logged_in?' do
+    it 'should be true if there is a current user' do
+      session[:user] = Factory(:user)
+      controller.logged_in?.should be_true
+    end
+
+    it 'should be false if there is no current user' do
+      session[:user] = nil
+      controller.logged_in?.should be_false
+    end
+  end
+
 end
--- spec/helpers/application_helper_spec.rb
+++ spec/helpers/application_helper_spec.rb
@@ -18,26 +18,6 @@ describe ApplicationHelper do
     end
   end
 
-  describe '#current_user' do
-    it 'should return the currently logged in user' do
-      user = Factory(:user)
-      session[:user] = user
-      current_user.should == user
-    end
-  end
-
-  describe '#logged_in?' do
-    it 'should be true if there is a current user' do
-      session[:user] = Factory(:user)
-      logged_in?.should be_true
-    end
-
-    it 'should be false if there is no current user' do
-      session[:user] = nil
-      logged_in?.should be_false
-    end
-  end
-
   describe '#gravatar_for_user' do
     it 'should return a gravatar url that has the md5 of the email' do
       user = Factory(:user)
