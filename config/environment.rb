6160de4229f21d261d0ec146ff846ca52f767a98
David Dollar <ddollar@gmail.com>
2009-04-01 19:09:04 -0400

    removing cucumber for now

--- config/environment.rb
+++ config/environment.rb
@@ -12,7 +12,7 @@ Rails::Initializer.run do |config|
   config.gem 'thoughtbot-shoulda',      :version => '>= 2.10.1', :lib => 'shoulda'
   config.gem 'thoughtbot-factory_girl', :version => '>= 1.2.0',  :lib => 'factory_girl'
   config.gem 'webrat',                  :version => '>= 0.4.0'
-  config.gem 'cucumber',                :version => '>= 0.2.2'
   config.gem 'rspec',                   :version => '>= 1.2.0',  :lib => false
   config.gem 'rspec-rails',             :version => '>= 1.2.0',  :lib => false
+
 end
