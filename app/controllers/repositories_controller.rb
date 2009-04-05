0ee353995e0d3c9429bc00546b9b02438c713e98
David Dollar <ddollar@gmail.com>
2009-04-04 21:26:06 -0400

    initial repositories controller

--- app/controllers/repositories_controller.rb
+++ app/controllers/repositories_controller.rb
@@ -3,6 +3,35 @@ class RepositoriesController < ApplicationController
   tab :repositories
 
   def index
+    @repositories = Repository.all
+  end
+
+  def new
+    @repository = Repository.new
+  end
+
+  def create
+    @repository = Repository.new(params[:repository])
+    if @repostiory.save
+      flash[:success] = 'Repository created.'
+      redirect_to repositories_path
+    else
+      render :edit
+    end
+  end
+
+  def edit
+    @repository = Repository.find(params[:id])
+  end
+
+  def update
+    @repository = Repository.find(params[:id])
+    if @repository.update_attributes(params[:repository])
+      flash[:success] = 'Repository updated.'
+      redirect_to repositories_path
+    else
+      render :edit
+    end
   end
 
 end
