8f31645af6392e88d4f63d8ae4c8026d47bc6a90
David Dollar <ddollar@gmail.com>
2009-04-08 19:11:50 -0400

    refactor repositories

--- spec/factories/repository.rb
+++ spec/factories/repository.rb
@@ -2,15 +2,15 @@
 #
 # Table name: repositories
 #
-#  id         :integer         not null, primary key
-#  name       :string(255)     not null
-#  path       :string(255)     not null
-#  public     :boolean         not null
-#  created_at :datetime
-#  updated_at :datetime
+#  id          :integer         not null, primary key
+#  description :string(255)     not null
+#  name        :string(255)     not null
+#  public      :boolean         not null
+#  created_at  :datetime
+#  updated_at  :datetime
 
 Factory.define :repository do |f|
-  f.sequence(:name) { |n| "Repository #{n}" }
-  f.sequence(:path) { |n| "repository#{n}"  }
-  f.public          true
+  f.sequence(:name)        { |n| "repository#{n}" }
+  f.sequence(:description) { |n| "Repository #{n}"  }
+  f.public                 true
 end
