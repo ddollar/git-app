981aced49137437c2ba300a0f7a619f33aaa8d95
David Dollar <ddollar@gmail.com>
2009-10-15 13:42:39 -0400

    add schema

---
+++ db/schema.rb
@@ -0,0 +1,41 @@
+# This file is auto-generated from the current state of the database. Instead of editing this file, 
+# please use the migrations feature of Active Record to incrementally modify your database, and
+# then regenerate this schema definition.
+#
+# Note that this schema.rb definition is the authoritative source for your database schema. If you need
+# to create the application database on another system, you should be using db:schema:load, not running
+# all the migrations from scratch. The latter is a flawed and unsustainable approach (the more migrations
+# you'll amass, the slower it'll run and the greater likelihood for issues).
+#
+# It's strongly recommended to check this file into your version control system.
+
+ActiveRecord::Schema.define(:version => 20090328025057) do
+
+  create_table "repositories", :force => true do |t|
+    t.string   "name",        :null => false
+    t.string   "description", :null => false
+    t.boolean  "public",      :null => false
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "repository_accesses", :force => true do |t|
+    t.integer  "repository_id", :null => false
+    t.integer  "user_id",       :null => false
+    t.boolean  "writable",      :null => false
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "users", :force => true do |t|
+    t.string   "email",                                 :null => false
+    t.string   "name",                                  :null => false
+    t.string   "encrypted_password",                    :null => false
+    t.string   "salt",                                  :null => false
+    t.text     "ssh_key",                               :null => false
+    t.boolean  "admin",              :default => false, :null => false
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+end
